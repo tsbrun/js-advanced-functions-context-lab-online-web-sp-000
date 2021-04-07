72ada52b6a35b86ef9da56c5c99721eb783000cb
TSephanya Brun <tsbrun@icloud.com>
2021-04-07 14:31:54 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,63 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employee) {
+    let employeeRecord = {}
+    employeeRecord.firstName = employee[0]
+    employeeRecord.familyName = employee[1]
+    employeeRecord.title = employee[2]
+    employeeRecord.payPerHour = employee[3]
+    employeeRecord.timeInEvents = []
+    employeeRecord.timeOutEvents = []
+    return employeeRecord
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(e => createEmployeeRecord(e))
+}
+
+let createTimeInEvent = function (date) {
+    let timeInEvent = {}
+    timeInEvent.type = "TimeIn"
+    timeInEvent.hour = parseInt(date.split(' ')[1])
+    timeInEvent.date = date.split(' ')[0]
+    this.timeInEvents.push(timeInEvent)
+    return this
+}
+
+let createTimeOutEvent = function (date) {
+    let timeOutEvent = {}
+    timeOutEvent.type = "TimeOut"
+    timeOutEvent.hour = parseInt(date.split(' ')[1])
+    timeOutEvent.date = date.split(' ')[0]
+    this.timeOutEvents.push(timeOutEvent)
+    return this
+}
+
+let hoursWorkedOnDate = function (date) {
+    let timeIn = this.timeInEvents.filter(t => t.date === date)[0]
+    let timeOut = this.timeOutEvents.filter(t => t.date === date)[0]
+    let hoursWorked = (timeOut.hour - timeIn.hour) / 100
+    return hoursWorked
+}
+
+
+let wagesEarnedOnDate = function (date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour 
+}
+
+function calculatePayroll(employees) {
+    // calculate #allWagesFor each employee
+    // reduce into a single sum   
+    return employees.reduce((payroll, employee) => {
+        return payroll += allWagesFor.call(employee)
+        // seems redundant because of implicit returns in Ruby, but you have to return in here, too, so the parent function has a value to return
+    }, 0)
+    // before, I forgot to set the initVal to 0, so it was the first object in the array, which was messing up my math and getting the function to return undefined (because you can't add objects and numbers)
+}
+
+function findEmployeeByFirstName(employees, firstName) {
+    return employees.filter(emp => emp.firstName === firstName)[0]
+}
+
